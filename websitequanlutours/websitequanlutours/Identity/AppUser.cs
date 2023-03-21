a33028ba42a24bbff333c24535e66105db61dea2
PHILONG2212 <nguyenphilong747@gmail.com>
2023-03-21 08:57:37 +0700

    chinh sua moi

---
+++ websitequanlutours/websitequanlutours/Identity/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace websitequanlutours.Identity
+{
+    public class AppUser : IdentityUser
+    {
+        public DateTime? NgaySinh { get; set; }
+        public string DiaChi { get; set; }
+
+        public string Mobile { get; set; }
+    }
+}
