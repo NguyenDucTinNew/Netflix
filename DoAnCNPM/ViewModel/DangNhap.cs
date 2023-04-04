c0688bf9d6b129657045fd9da94b12651bc063ff
Dohuuphi <106214153+Dohuuphi@users.noreply.github.com>
2023-04-04 22:22:18 +0700

    View Model

---
+++ DoAnCNPM/ViewModel/DangNhap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace websitequanlutours.ViewModel
+{
+    public class DangNhap
+    {
+        [Required(ErrorMessage = "Không để trống Username")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Không để trống Password")]
+        public string Password { get; set; } 
+    }
+}
