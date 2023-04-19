fa7214805da5c25b9d11c0f33c349df134a66b4f
PHILONG2212 <100652897+PHILONG2212@users.noreply.github.com>
2023-04-19 10:57:05 +0700

    Add files via upload

---
+++ Identity/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using websitequanlutours.Models;
+
+namespace websitequanlutours.Identity
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+
+
+
+     
+      
+        
+
+        public AppDbContext() : base("MyConnectionString") { }
+
+
+        
+    }
+}
---
+++ Identity/AppUser.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using websitequanlutours.Models;
+using websitequanlutours.ViewModel;
+
+namespace websitequanlutours.Identity
+{
+    public class AppUser : IdentityUser
+    {
+        
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int MaUser { get; set; } // Kiểu dữ liệu có thể khác tùy theo thiết kế của bạn
+        public DateTime? NgaySinh { get; set; }
+        public string DiaChi { get; set; }
+
+        public string Mobile { get; set; }
+
+        public string ImageFile { get; set; }
+
+        public int? MaHoaDon { get; set; }
+
+       public virtual HoaDon HoaDon { get; set; }
+
+    }
+  
+}
+
+   
