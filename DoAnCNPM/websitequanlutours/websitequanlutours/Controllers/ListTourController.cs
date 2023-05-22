2fc7cc0c07b9b98310f55054fefec818b7e8a4ee
NguyenDucTinNew <93468105+HuyLieu1603@users.noreply.github.com>
2023-05-22 17:06:42 +0700

    Danh Sách Tour

---
+++ DoAnCNPM/websitequanlutours/websitequanlutours/Controllers/ListTourController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace websitequanlutours.Controllers
+{
+    public class ListTourController : Controller
+    {
+        // GET: ListTour
+        public ActionResult TourList()
+        {
+            return View();
+        }
+    }
+}
